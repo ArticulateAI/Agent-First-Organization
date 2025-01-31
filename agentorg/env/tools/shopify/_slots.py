249b4ffcc5c7dfbb4dd5a5b1838341f6f4f3a17a
isaac-articulateai <isaac.song@arklex.ai>
2025-01-31 17:26:09 -0500

    clean up merge remanants

--- agentorg/env/tools/shopify/_slots.py
+++
@@ -1,8 +0,0 @@
-CART_ID = {
-    "name": "cart_id",
-    "type": "str",
-    "description": "The cart id, such as 'gid://shopify/Cart/2938501948327'.",
-    "prompt": "",
-    "required": True,
-},
-
